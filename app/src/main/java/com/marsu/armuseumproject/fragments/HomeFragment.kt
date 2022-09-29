dd1e20c91947ace1e174e34c20a448235f2fec83
Oskari Arponen <oskarar@metropolia.fi>
2022-09-29 14:15:44 +0300

    HomeFragment to fragments directory

--- app/src/main/java/com/marsu/armuseumproject/fragments/HomeFragment.kt
+++ app/src/main/java/com/marsu/armuseumproject/fragments/HomeFragment.kt
@@ -1,12 +1,12 @@
-package com.marsu.armuseumproject
+package com.marsu.armuseumproject.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import com.marsu.armuseumproject.R
 import com.marsu.armuseumproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
