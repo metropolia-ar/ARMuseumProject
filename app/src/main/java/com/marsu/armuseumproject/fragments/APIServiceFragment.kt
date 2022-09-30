6610c38c93b0267daf0f0ff38331adfb8d66f958
Oskari Arponen <oskarar@metropolia.fi>
2022-09-30 12:36:34 +0300

    soft keyboard hiding + disable search btn when loading

--- app/src/main/java/com/marsu/armuseumproject/fragments/APIServiceFragment.kt
+++ app/src/main/java/com/marsu/armuseumproject/fragments/APIServiceFragment.kt
@@ -1,11 +1,13 @@
 package com.marsu.armuseumproject.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.marsu.armuseumproject.adapters.ApiServiceAdapter
@@ -37,17 +39,20 @@ class APIServiceFragment : Fragment() {
         adapter = ApiServiceAdapter()
         adapter.setHasStableIds(true)
         layoutManager = LinearLayoutManager(activity)
-
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.recyclerView.setHasFixedSize(true)
         binding.recyclerView.layoutManager = layoutManager
+        apiServiceViewModel.getArts(false)
 
+        // Search button
         binding.button2.setOnClickListener {
             apiServiceViewModel.searchArtsWithInput()
+            val kb = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            kb.hideSoftInputFromWindow(view?.windowToken, 0)
         }
 
-        apiServiceViewModel.getArts(false)
+        // Recyclerview updates when fetching data from API
         apiServiceViewModel.artsList.observe(viewLifecycleOwner) { arts ->
             arts.let {
                 adapter.setData(it)
@@ -75,10 +80,12 @@ class APIServiceFragment : Fragment() {
                 if (!it) {
                     binding.progressBar.visibility = View.VISIBLE
                     binding.recyclerView.visibility = View.INVISIBLE
+                    binding.button2.isEnabled = false
                 }
                 else {
                     binding.progressBar.visibility = View.INVISIBLE
                     binding.recyclerView.visibility = View.VISIBLE
+                    binding.button2.isEnabled = true
                 }
             }
         }
