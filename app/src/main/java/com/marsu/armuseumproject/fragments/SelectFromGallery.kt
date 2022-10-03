07e63d4b0f29119a8922c2bbd3437e87fd2689ab
Jani Salo <Janikristian.salo@gmail.com>
2022-10-03 16:30:16 +0300

    Textfields for additional information of gallery images added

--- app/src/main/java/com/marsu/armuseumproject/fragments/SelectFromGallery.kt
+++ app/src/main/java/com/marsu/armuseumproject/fragments/SelectFromGallery.kt
@@ -3,17 +3,20 @@ package com.marsu.armuseumproject.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import com.google.android.material.textfield.TextInputEditText
 import com.marsu.armuseumproject.MyApp
 import com.marsu.armuseumproject.R
 import com.marsu.armuseumproject.SelectFromGalleryViewModel
@@ -26,6 +29,7 @@ class SelectFromGallery : Fragment() {
     private var imageUri: Uri? = null
     private var _binding: FragmentSelectFromGalleryBinding? = null
     private val binding get() = _binding!!
+
 
     companion object {
         private lateinit var viewModel: SelectFromGalleryViewModel
@@ -42,24 +46,38 @@ class SelectFromGallery : Fragment() {
         val button: Button = binding.ChooseImage
         val saveButton: Button = binding.saveButton
         val backButton: LinearLayout = binding.sfgBackButton
+        val titleEditText = binding.inputTitle
+        val artistEditText = binding.inputArtist
+        val departmentEditText = binding.inputDepartment
+        val classEditText = binding.inputClassification
+
 
         button.setOnClickListener {
             openGalleryForImage()
         }
         saveButton.setOnClickListener {
-            if (imageUri == null) {
+            if (imageUri == null || titleEditText.text.toString() == "") {
                 Toast.makeText(
                     MyApp.appContext,
                     getString(R.string.pickImageToast),
                     Toast.LENGTH_SHORT
                 ).show()
             } else {
-                insertToDatabase(viewModel, imageUri)
+                insertToDatabase(
+                    viewModel,
+                    imageUri,
+                    titleEditText.text.toString(),
+                    artistEditText.text.toString(),
+                    departmentEditText.text.toString(),
+                    classEditText.text.toString()
+                )
+                clearEditTexts(titleEditText, artistEditText, departmentEditText, classEditText)
             }
-
         }
 
-        backButton.setOnClickListener { view.findNavController().navigate(R.id.action_selectFromGallery_to_homeFragment) }
+        backButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_selectFromGallery_to_homeFragment)
+        }
 
         return view
     }
@@ -70,11 +88,44 @@ class SelectFromGallery : Fragment() {
         startActivityForResult(intent, REQUEST_CODE)
     }
 
-    private fun insertToDatabase(viewModel: SelectFromGalleryViewModel, uri: Uri?) {
+    private fun insertToDatabase(
+        viewModel: SelectFromGalleryViewModel,
+        uri: Uri?,
+        title: String,
+        artist: String,
+        department: String,
+        classification: String
+    ) {
         val objectID: Int = uri.hashCode() * -1
         Log.d("HASHCODE TEST", objectID.toString())
 
-        viewModel.insertImage(Artwork(objectID, uri.toString(), "", "", "", "", "", ""))
+        viewModel.insertImage(
+            Artwork(
+                objectID,
+                uri.toString(),
+                "",
+                department,
+                title,
+                artist,
+                classification
+            )
+        )
+    }
+
+    private fun clearEditTexts(
+        title: TextInputEditText,
+        artist: TextInputEditText,
+        department: TextInputEditText,
+        classification: TextInputEditText
+    ) {
+        title.setText("")
+        artist.setText("")
+        department.setText("")
+        classification.setText("")
+        title.clearFocus()
+        artist.clearFocus()
+        department.clearFocus()
+        classification.clearFocus()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -85,4 +136,5 @@ class SelectFromGallery : Fragment() {
             imgView.setImageURI(imageUri)
         }
     }
+
 }
