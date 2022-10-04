46d501dfbe15a1fe6cc4223344fb41e6e2fabbb1
Jani Salo <Janikristian.salo@gmail.com>
2022-10-04 12:25:24 +0300

    Default image added, textfields cleared when artwork is saved, unfocusing textfields and hiding keyboard when clicking screen

--- app/src/main/java/com/marsu/armuseumproject/fragments/SelectFromGallery.kt
+++ app/src/main/java/com/marsu/armuseumproject/fragments/SelectFromGallery.kt
@@ -1,5 +1,7 @@
 package com.marsu.armuseumproject.fragments
 
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -9,12 +11,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.findNavController
 import com.google.android.material.textfield.TextInputEditText
 import com.marsu.armuseumproject.MyApp
@@ -24,6 +28,7 @@ import com.marsu.armuseumproject.database.Artwork
 import com.marsu.armuseumproject.databinding.FragmentSelectFromGalleryBinding
 
 const val REQUEST_CODE = 200
+
 
 class SelectFromGallery : Fragment() {
     private var imageUri: Uri? = null
@@ -41,8 +46,8 @@ class SelectFromGallery : Fragment() {
     ): View? {
         _binding = FragmentSelectFromGalleryBinding.inflate(inflater, container, false)
         viewModel = SelectFromGalleryViewModel()
-        val view = binding.root
 
+        val view = binding.root
         val button: Button = binding.ChooseImage
         val saveButton: Button = binding.saveButton
         val backButton: LinearLayout = binding.sfgBackButton
@@ -50,8 +55,24 @@ class SelectFromGallery : Fragment() {
         val artistEditText = binding.inputArtist
         val departmentEditText = binding.inputDepartment
         val classEditText = binding.inputClassification
+        val imgView = binding.imageFromGallery
+        val constraint = binding.ConstraintLayout
 
+        imgView.setImageResource(R.drawable.image_not_found)
 
+        constraint.setOnClickListener {
+            clearFocuses(
+                titleEditText,
+                artistEditText,
+                departmentEditText,
+                classEditText,
+                constraint
+            )
+        }
+
+        imgView.setOnClickListener {
+            openGalleryForImage()
+        }
         button.setOnClickListener {
             openGalleryForImage()
         }
@@ -71,13 +92,20 @@ class SelectFromGallery : Fragment() {
                     departmentEditText.text.toString(),
                     classEditText.text.toString()
                 )
-                clearEditTexts(titleEditText, artistEditText, departmentEditText, classEditText)
+                clearEditTexts(
+                    titleEditText,
+                    artistEditText,
+                    departmentEditText,
+                    classEditText,
+                    imgView
+                )
             }
         }
 
         backButton.setOnClickListener {
             view.findNavController().navigate(R.id.action_selectFromGallery_to_homeFragment)
         }
+
 
         return view
     }
@@ -116,7 +144,8 @@ class SelectFromGallery : Fragment() {
         title: TextInputEditText,
         artist: TextInputEditText,
         department: TextInputEditText,
-        classification: TextInputEditText
+        classification: TextInputEditText,
+        imgView: ImageView
     ) {
         title.setText("")
         artist.setText("")
@@ -126,6 +155,7 @@ class SelectFromGallery : Fragment() {
         artist.clearFocus()
         department.clearFocus()
         classification.clearFocus()
+        imgView.setImageResource(R.drawable.image_not_found)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -137,4 +167,23 @@ class SelectFromGallery : Fragment() {
         }
     }
 
+    private fun closeKeyBoard(view: View) {
+        val imm: InputMethodManager =
+            view.context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun clearFocuses(
+        title: TextInputEditText,
+        artist: TextInputEditText,
+        department: TextInputEditText,
+        classification: TextInputEditText,
+        view: View
+    ) {
+        title.clearFocus()
+        artist.clearFocus()
+        department.clearFocus()
+        classification.clearFocus()
+        closeKeyBoard(view)
+    }
 }
