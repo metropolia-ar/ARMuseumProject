dccac0cc72c541d4e676878a0055b04a9dab7422
Sirja Kosonen <sirja.kosonen@gmail.com>
2022-10-11 10:29:25 +0300

    Storing lastFive variable to shared preferences
    lastFive variable handling works almost correctly now. Getting rid of duplicates does not work yet for some reason.

--- app/src/main/java/com/marsu/armuseumproject/fragments/ArSelection.kt
+++ app/src/main/java/com/marsu/armuseumproject/fragments/ArSelection.kt
@@ -1,5 +1,6 @@
 package com.marsu.armuseumproject.fragments
 
+import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -10,18 +11,27 @@ import android.view.ViewGroup
 import androidx.core.net.toUri
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.marsu.armuseumproject.MyApp
 import com.marsu.armuseumproject.adapters.ArSelectionAdapter
 import com.marsu.armuseumproject.R
 import com.marsu.armuseumproject.databinding.FragmentArSelectionBinding
 import com.marsu.armuseumproject.viewmodels.ArSelectionViewModel
+import java.lang.reflect.Type
+
+const val SHARED_KEY = "LAST_FIVE"
 
 class ArSelection : Fragment() {
 
     private lateinit var arSelectionViewModel: ArSelectionViewModel
     private lateinit var adapter: ArSelectionAdapter
-    private lateinit var layoutManager : LinearLayoutManager
+    private lateinit var layoutManager: LinearLayoutManager
     private var _binding: FragmentArSelectionBinding? = null;
     private val binding get() = _binding!!
+
+    private var lastFive = mutableListOf<Int>() // initiate variable
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,12 +46,48 @@ class ArSelection : Fragment() {
         _binding = FragmentArSelectionBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        val sharedPreferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        val json = sharedPreferences?.getString(SHARED_KEY, null)
+        val type: Type = object : TypeToken<List<Int>>() {}.type
+        if (json != null) {
+            lastFive = Gson().fromJson<MutableList<Int>>(json, type)
+        }
+
         binding.startArButton.setOnClickListener { navigateToArActivity(view) }
 
         adapter = ArSelectionAdapter()
         adapter.setHasStableIds(true)
         layoutManager = LinearLayoutManager(activity)
         adapter.onItemClick = { artwork ->
+
+
+            if (!lastFive.contains(artwork.objectID) || lastFive.isEmpty()) {
+                lastFive.add(
+                    0,
+                    artwork.objectID
+                ) // add the newest to the first element of list, previous ones get pushed one more forward
+            } else {
+                val index = lastFive.indexOf(artwork.objectID)
+                lastFive.drop(index)
+                lastFive.add(
+                    0,
+                    artwork.objectID
+                ) // add the newest only after dropping out the very same id or int from the list
+            }
+
+            if (lastFive.size > 5) {
+                lastFive.removeLast() // keeps only 5 most recent in the list
+            }
+
+            val storedLastFive = Gson().toJson(lastFive) // convert list to storable json
+            val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+            if (sharedPref != null) {
+                with(sharedPref.edit()) {
+                    putString(SHARED_KEY, storedLastFive)
+                    apply()
+                }
+            }
+
             binding.chosenTitle.text = artwork.title
             binding.chosenArtist.text = artwork.artistDisplayName
             arSelectionViewModel.imageUri.postValue(artwork.primaryImage.toUri())
--- app/src/main/java/com/marsu/armuseumproject/fragments/HomeFragment.kt
+++ app/src/main/java/com/marsu/armuseumproject/fragments/HomeFragment.kt
@@ -1,20 +1,23 @@
 package com.marsu.armuseumproject.fragments
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
 import com.google.gson.Gson
-import com.marsu.armuseumproject.R
+import com.marsu.armuseumproject.MyApp
 import com.marsu.armuseumproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private val lastFive = mutableListOf<Int>() // initiate variable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,36 +32,25 @@ class HomeFragment : Fragment() {
         // Checks if the id or uri already exist in the list
         // If it does, removes the old one from the collection before adding it again as the latest
 
-        var idoruri = 0 // placeholder for the actual data to be stored
-        val lastFive = mutableListOf<Int>() // initiate variable
-        if (!lastFive.contains(idoruri) || lastFive.isEmpty()) {
-            lastFive.add(0, idoruri) // add the newest to the first element of list, previous ones get pushed one more forward
-        } else {
-            val index = lastFive.indexOf(idoruri)
-            lastFive.drop(index)
-            lastFive.add(0, idoruri) // add the newest only after dropping out the very same id or int from the list
-        }
-
-        if (lastFive.size > 5) {
-            lastFive.removeLast() // keeps only 5 most recent in the list
-        }
-
         // TODO: Place the logic to the AR Selection
         // Logic to store list into shared preference
         // Accessing .getSharedPreferences function to retrieve or modify, requires context
         // Make the SHARED_KEY a const val outside the ar selection class, like request_code in sfg
-        val SHARED_KEY = "LAST_FIVE"
-        var storedLastFive = Gson().toJson(lastFive) // convert list to storable json
-        val sharedPreferences: SharedPreferences? = context?.getSharedPreferences("PREFERENCE_NAME", 0) // remove nullable when in correct place
-        val editor = sharedPreferences?.edit()
-        editor?.putString(SHARED_KEY,storedLastFive)
-        editor?.apply()
+
 
         // TODO: Retrieve stored data from shared preferences and show in this view
         // First access to the shared preferences
         // Retrieve LAST_FIVE
         // Convert back to list
-        
+        val sharedPreferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        if (sharedPreferences != null) {
+            val haettu = sharedPreferences.getString(SHARED_KEY, "storedLastFive")
+            if (haettu != null) {
+                Log.d("TESTING", haettu)
+            }
+        }
+
+
         // TODO: Selvitä miten näytät listasta haetut tiedot
         // Recyclerin todennäköisesti tarttee, koska pienemmällä näytöllä voi tarvita scrollable ominaisuuden
         // Mutta miten recyclerin kanssa saa alkuun näytettyä vähemmän kuin 5 viimeisintä?
